ee3faaf6a2c6e4ab42957dd6f94489d8ec85bd2c
Cédric Skwar <cdrc@skwar.me>
2023-04-28 13:25:44 +0200

    added a doc comment for PrintError

--- utils.go
+++ utils.go
@@ -15,6 +15,12 @@ func (m *MessengerUtils) PrintInfo(message string) {
 	}
 }
 
+// PrintError formats and prints an error message to standard output.
+// @param {string} message - A string representing the error message to display.
+// @param {Error} err - An error object providing more information about the error.
+// The function formats the error message with the provided message and error object,
+// then prints it to the console with a bold "ERROR:" label using ANSI escape codes.
+// @returns {void}
 func PrintError(message string, err error) {
 	fmt.Printf("\033[1mERROR:\033[0m %s: %v\n", message, err)
 }
