bfec6f342c0bf205e9d548d2103b4c97ed98c588
Cédric Skwar <cdrc@skwar.me>
2023-04-28 13:20:11 +0200

    added a doc comment for PrintInfo

--- utils.go
+++ utils.go
@@ -6,6 +6,9 @@ type MessengerUtils struct {
 	verbose bool
 }
 
+// PrintInfo prints the provided message to the console with the prefix "INFO" in bold, if the MessengerUtils's verbose flag is set to true.
+// @param {string} message - The message to print to the console.
+// @returns {void}
 func (m *MessengerUtils) PrintInfo(message string) {
 	if m.verbose {
 		fmt.Printf("\033[1m%s\033[0m: %s\n", "INFO", message)
