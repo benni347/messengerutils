5656676b3111d421a96823ab82dbf73da57ba4a3
Cédric Skwar <cdrc@skwar.me>
2023-05-01 08:51:38 +0200

    exported the Verbose field in MessengerUtils struct

--- utils.go
+++ utils.go
@@ -3,14 +3,14 @@ package messengerutils
 import "fmt"
 
 type MessengerUtils struct {
-	verbose bool
+	Verbose bool
 }
 
 // PrintInfo prints the provided message to the console with the prefix "INFO" in bold, if the MessengerUtils's verbose flag is set to true.
 // @param {string} message - The message to print to the console.
 // @returns {void}
 func (m *MessengerUtils) PrintInfo(message string) {
-	if m.verbose {
+	if m.Verbose {
 		fmt.Printf("\033[1m%s\033[0m: %s\n", "INFO", message)
 	}
 }
